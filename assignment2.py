dc4b295e164fad63211de525ce49a3a8f30a698f
Alexis Manosca <alexismanosca@gmail.com>
2025-04-07 14:23:52 +1200

    Added a fitness function

---
+++ assignment2.py
@@ -0,0 +1,234 @@
+from search import *
+from random import randint
+from assignment2aux import *
+
+def read_tiles_from_file(filename):
+    lines = [line.rstrip('\n') for line in open(filename, 'r').readlines()]
+    character_to_tile = {' ': (), 'i': (0,), 'L': (0, 1), 'I': (0, 2), 'T': (0, 1, 2)}
+    return tuple(tuple(character_to_tile[character] for character in line) for line in lines)
+
+class KNetWalk(Problem):
+    def __init__(self, tiles):
+        if type(tiles) is str:
+            self.tiles = read_tiles_from_file(tiles)
+        else:
+            self.tiles = tiles
+        self.height = len(self.tiles)
+        self.width = len(self.tiles[0])
+        self.max_fitness = sum(sum(len(tile) for tile in row) for row in self.tiles)
+        super().__init__(self.generate_random_state())
+
+    def generate_random_state(self):
+        height = len(self.tiles)
+        width = len(self.tiles[0])
+        return [randint(0, 3) for _ in range(height) for _ in range(width)]
+
+    def actions(self, state):
+        height = len(self.tiles)
+        width = len(self.tiles[0])
+        return [(i, j, k) for i in range(height) for j in range(width) for k in [0, 1, 2, 3] if state[i * width + j] != k]
+
+    def result(self, state, action):
+        pos = action[0] * len(self.tiles[0]) + action[1]
+        return state[:pos] + [action[2]] + state[pos + 1:]
+
+    def goal_test(self, state):
+        return self.value(state) == self.max_fitness
+
+    def value(self, state):
+        # Task 1
+        # Return an integer fitness value of a given state.
+        # Replace the line below with your code.
+
+        # Create fitness function
+        fitness_function = 0
+
+        # Create a dictionary for the valid connection types
+        valid_connections = {0: 2, 1: 3, 2: 0, 3: 1}
+
+        # Loop through the state
+        for i in range(self.height):
+            for j in range(self.width):
+                # (i,j) is the current examining tile and stores what type of tile it is and what connections it has
+                current_tile = self.tiles[i][j]
+
+                # Current state stores the orientation of the current tile (how many times its rotated counterclockwise)
+                current_state = state[i * self.width + j]
+
+                # Checks if the right neighbouring tile (i, j + 1) exists 
+                if j < self.width - 1:
+                    # Right tile exists within the boundary of the game board
+                    right_tile = self.tiles[i][j + 1]
+
+                    # Checks the connection between the current tile and the right tile
+                    for current_pipe_direction in current_tile:
+                        for right_pipe_direction in right_tile:
+                            # Checks if the current tile has a connection and if so, add to the fitness function
+                            if valid_connections[current_pipe_direction] in right_tile:
+                                fitness_function += 1
+                
+                # Checks if the bottom neighbouring tile (i + 1, j) exists
+                if i < self.height - 1:
+                    # Bottom tile exists within the boundary of the game board
+                    bottom_tile = self.tiles[i + 1][j]
+
+                    # Checks the connection between the current tile and the bottom tile
+                    for current_pipe_direction in current_tile:
+                        for bottom_pipe_direction in bottom_tile:
+                            # Checks if the current tile has a connection and if so, add to the fitness function
+                            if valid_connections[current_pipe_direction] in bottom_tile:
+                                fitness_function += 1
+
+                # Checks if the left neighbouring tile (i, j - 1) exists
+                if j > 0:
+                    # Left tile exists within the boundary of the game board
+                    left_tile = self.tiles[i][j - 1]
+
+                    # Checks the connection between the current tile and the left tile
+                    for current_pipe_direction in current_tile:
+                        for left_pipe_direction in left_tile:
+                            # Checks if the current tile has a connection and if so, add to the fitness function
+                            if valid_connections[current_pipe_direction] in left_tile:
+                                fitness_function += 1
+                
+                # Checks if the top neighbouring tile (i - 1, j) exists
+                if i > 0:
+                    # Top tile exists within the boundary of the game board
+                    top_tile = self.tiles[i - 1][j]
+
+                    # Checks the connection between the current tile and the top tile
+                    for current_pipe_direction in current_tile:
+                        for top_pipe_direction in top_tile:
+                            # Checks if the current tile has a connection and if so, add to the fitness function
+                            if valid_connections[current_pipe_direction] in top_tile:
+                                fitness_function += 1
+
+        # Return the fitness function
+        return fitness_function
+                
+
+
+# Task 2
+# Configure an exponential schedule for simulated annealing.
+sa_schedule = exp_schedule(k=20, lam=0.005, limit=100)
+
+# Task 3
+# Configure parameters for the genetic algorithm.
+pop_size = None
+num_gen = 1000
+mutation_prob = 0.1
+
+def local_beam_search(problem, population):
+    # Task 4
+    # Implement local beam search.
+    # Return a goal state if found in the population.
+    # Return the fittest state in the population if the next population contains no fitter state.
+    # Replace the line below with your code.
+    raise NotImplementedError
+
+def stochastic_beam_search(problem, population, limit=1000):
+    # Task 5
+    # Implement stochastic beam search.
+    # Return a goal state if found in the population.
+    # Return the fittest state in the population if the generation limit is reached.
+    # Replace the line below with your code.
+    raise NotImplementedError
+
+if __name__ == '__main__':
+
+    network = KNetWalk('assignment2config.txt')
+    visualise(network.tiles, network.initial)
+
+    # Task 1 test code
+    run = 0
+    method = 'hill climbing'
+    while True:
+        network = KNetWalk('assignment2config.txt')
+        state = hill_climbing(network)
+        if network.goal_test(state):
+            break
+        else:
+            print(f'{method} run {run}: no solution found')
+            print(f'best state fitness {network.value(state)} out of {network.max_fitness}')
+            visualise(network.tiles, state)
+        run += 1
+    print(f'{method} run {run}: solution found')
+    visualise(network.tiles, state)
+
+    # Task 2 test code
+    '''
+    run = 0
+    method = 'simulated annealing'
+    while True:
+        network = KNetWalk('assignment2config.txt')
+        state = simulated_annealing(network, schedule=sa_schedule)
+        if network.goal_test(state):
+            break
+        else:
+            print(f'{method} run {run}: no solution found')
+            print(f'best state fitness {network.value(state)} out of {network.max_fitness}')
+            visualise(network.tiles, state)
+        run += 1
+    print(f'{method} run {run}: solution found')
+    visualise(network.tiles, state)
+    '''
+
+    # Task 3 test code
+    '''
+    run = 0
+    method = 'genetic algorithm'
+    while True:
+        network = KNetWalk('assignment2config.txt')
+        height = len(network.tiles)
+        width = len(network.tiles[0])
+        state = genetic_algorithm([network.generate_random_state() for _ in range(pop_size)], network.value, [0, 1, 2, 3], network.max_fitness, num_gen, mutation_prob)
+        if network.goal_test(state):
+            break
+        else:
+            print(f'{method} run {run}: no solution found')
+            print(f'best state fitness {network.value(state)} out of {network.max_fitness}')
+            visualise(network.tiles, state)
+        run += 1
+    print(f'{method} run {run}: solution found')
+    visualise(network.tiles, state)
+    '''
+
+    # Task 4 test code
+    '''
+    run = 0
+    method = 'local beam search'
+    while True:
+        network = KNetWalk('assignment2config.txt')
+        height = len(network.tiles)
+        width = len(network.tiles[0])
+        state = local_beam_search(network, [network.generate_random_state() for _ in range(100)])
+        if network.goal_test(state):
+            break
+        else:
+            print(f'{method} run {run}: no solution found')
+            print(f'best state fitness {network.value(state)} out of {network.max_fitness}')
+            visualise(network.tiles, state)
+        run += 1
+    print(f'{method} run {run}: solution found')
+    visualise(network.tiles, state)
+    '''
+
+    # Task 5 test code
+    '''
+    run = 0
+    method = 'stochastic beam search'
+    while True:
+        network = KNetWalk('assignment2config.txt')
+        height = len(network.tiles)
+        width = len(network.tiles[0])
+        state = stochastic_beam_search(network, [network.generate_random_state() for _ in range(100)])
+        if network.goal_test(state):
+            break
+        else:
+            print(f'{method} run {run}: no solution found')
+            print(f'best state fitness {network.value(state)} out of {network.max_fitness}')
+            visualise(network.tiles, state)
+        run += 1
+    print(f'{method} run {run}: solution found')
+    visualise(network.tiles, state)
+    '''
